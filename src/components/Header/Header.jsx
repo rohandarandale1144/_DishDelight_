73e3b97394ec9b3afbb02f74a40d4c4a947a631d
Rohan Darandale <rohan.darandale21@pccoepune.org>
2024-07-31 14:11:55 +0530

    Frontend Updated

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-content">
+        <h2>Order your favourite food here</h2>
+        <p>Order now for a delicious dining experience! Simply select your favorites from our menu and enjoy hassle-free delivery to your doorstep. It's that easy!</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
